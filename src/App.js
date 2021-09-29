23f2fd4084dc121421f185407118cfc97cd259e2
tamimhossen <md.tamimhossen1767@gmail.com>
2021-09-29 14:20:15 +0600

    Material ui practice done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button";
+import News from "./components/News/News";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [articles, setArtcles] = useState([]);
+  useEffect(() => {
+    const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-08-29&sortBy=publishedAt&apiKey=287d6a5b4c3b48a49ed26154ff6c9b67';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArtcles(data.articles))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
